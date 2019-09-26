c7b68d4852abf4eb390ac0e0884006dcf8ea6f74
ekisalman03 <55838966+ekisalman03@users.noreply.github.com>
2019-09-26 19:42:17 +0700

    Add files via upload

---
+++ PBO1-10118041-Latihan6-KambingStatic/src/kambingstatic.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 
+ * Nama      : Eki salman sya'bani
+ * Kelas     : PBO1
+ * NIM       : 10118041
+ * Deskripsi : Penggunaan variabel static dan konstanta
+ */
+public class kambingstatic {
+    //NamaKambing sebagai konstanta
+    public static final String Nama_Kambing ="Midun";
+    
+    public static void main(String[] args) {
+        mamalia.JumlahKambing = 485000;
+        System.out.println(Nama_Kambing + " Memiliki kambing sebanyak " +mamalia.JumlahKambing);
+        
+    }
+}
---
+++ PBO1-10118041-Latihan6-KambingStatic/src/mamalia.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 
+ * Nama      : Eki salman sya'bani
+ * Kelas     : PBO1
+ * NIM       : 10118041
+ * Deskripsi : Penggunaan variabel static dan konstanta
+ */
+public class mamalia {
+    //Variabel jumlahkambing dideklarasikan sebagai statik
+    public static int JumlahKambing;
+}

b3a220b11b7a045e4e2c32c8b004606155955103
ekisalman03 <55838966+ekisalman03@users.noreply.github.com>
2019-09-26 19:37:28 +0700

    Add files via upload

---
+++ PBO1-10118041-Latihan1-Biodata/src/biodata.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author
+ * Nama      : Eki salman sya'bani
+ * Kelas     : PBO1
+ * NIM       : 10118041
+ * Deskripsi : Menampilkan biodata
+ */
+public class biodata {
+    
+    public static void main(String[] args){
+        System.out.println("Biodat" +"\n"+ "----------------------------");
+        System.out.println("Nama\t\t:Eki salman sya'bani");
+        System.out.println("Alamat\t\t:Dago barat");
+        System.out.println("Tempat Lahir\t:Cianjur");
+        System.out.println("Tanggal Lahir\t:23 november 1999");
+        System.out.println("----------------------------");
+        System.out.println("Hobi\t\t:Nonton Youtube");
+        System.out.println("Makes\t\t:Kangkung");
+        System.out.println("Mikes\t\t:Susu murni");
+    }
+   
+}
